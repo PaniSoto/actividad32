2fd153dd80a15da80107ae421c6382b7823e5109
Miguel040404 <mmaldonadocobo4@gmail.com>
2024-10-18 19:31:04 +0200

    Cambio sin actualizar

---
+++ src/app/1_2_7_exencion_de_la_fct/page.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+
+export const metadata = {
+  title: "Información general: convalidaciones, exenciones y títulos y pollos",
+};
+
+
+export default function Home() {
+  return (
+    <div>
+    <h1 className="text-9xl border-bottom-[3px] border-blue-500 font-black border-b-[3px] text-[rgb(0,0,255,0.5)] font-black ">Holi
+    </h1>
+    </div>
+  );
+}
