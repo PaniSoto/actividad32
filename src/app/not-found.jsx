c4571497ec16f2aeab2980a8a5c3953fc5c2e83a
Miguel040404 <mmaldonadocobo4@gmail.com>
2024-10-18 19:28:37 +0200

    pruebaInicio

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+function NotFound() {
+    return ( 
+        <div className="grid place-content-center h-full">
+            <p className="text-blue-200 text-9xl font-bold">
+            Not Found
+            </p>
+        </div>
+        
+     );
+}
+
+export default NotFound;
